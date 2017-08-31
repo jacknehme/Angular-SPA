769012d358dc39cd0dc9a92443fc9debb1c99042
Jack Nehme <jack.nehme@gmail.com>
2017-08-30 19:48:44 -0700

    Added images

--- Framework/app/directives/wwaDashboardDirective.js
+++ Framework/app/directives/wwaDashboardDirective.js
@@ -27,15 +27,16 @@
                         sizeX: 3,
                         sizeY: 3,
                         row: 0,
-                        col: 0
+                        col: 0,
+                        template: '<wwa-temperature></wwa-temperature>'
                     },
-                    {
-                        title: 'Second',
-                        sizeX: 2,
-                        sizeY: 4,
-                        row: 0,
-                        col: 5
-                    },
+                    //{
+                    //    title: 'Second',
+                    //    sizeX: 2,
+                    //    sizeY: 4,
+                    //    row: 0,
+                    //    col: 5
+                    //},
                     
                 ];
             }
---
+++ Framework/ext-modules/jnDashboard/jnWidgetBodyDirective.js
@@ -0,0 +1,19 @@
+﻿(function () {
+
+    "use strict";
+
+    angular.module("jnDashboard").directive("jnWidgetBody", ['$compile', jnWidgetBody]);
+
+    function jnWidgetBody($compile) {
+        return {
+            //scope: inheireted
+            templateUrl: 'ext-modules/jnDashboard/jnWidgetBodyTemplate.html',
+            link: function (scope, element, attrs) {
+                var newElement = angular.element(scope.item.template);
+                element.append(newElement);
+                $compile(newElement)(scope);
+            }
+        }
+    };
+
+})();
